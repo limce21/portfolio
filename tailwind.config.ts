a473a769780ff54227fc63f4a9fe50d1cec06c80
ChaenLim <limce21@naver.com>
2023-09-05 10:25:07 +0900

    style: 폰트, 색상 커스텀

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+			}
+		},
+		colors: {
+			primary: '#BB2649',
+			lightPink: '#FBE9EF',
+			brightRed: '#DF4A65',
+			deepTurquoise: '#007975',
+			dustyRose: '#AC7085',
+			white: '#FFFFFF',
+			black: '#000000'
+		},
+		fontFamily: {
+			sans: ['SCoreDream', 'sans-serif'],
+			title: ['PyeongChangPeace']
+		}
+	},
+	plugins: []
+};
+export default config;
